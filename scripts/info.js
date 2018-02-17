ff090a15426eb5dc8cd8602a91106c25ab84ecb5
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2018-02-17 15:50:06 -0500

    Highlight diagram on click

--- scripts/info.js
+++ scripts/info.js
@@ -6,7 +6,13 @@ var showInfo = (x) => {
   for (let info of document.getElementsByClassName('info')) {
     info.classList.remove('active')
   }
+
+  for (let node of document.getElementsByClassName('node')) {
+    node.classList.remove('active-node')
+  }
+
   document.getElementById(x + '-info').classList.add('active')
+  document.getElementById(x).classList.add('active-node')
 }
 
-document.getElementById('button').onclick = () => showInfo('default')
+document.getElementById('default').onclick = () => showInfo('default')
